324b2396222fa38f731c7e762088eb50a21cc665
David Azevedo <davidhazevedog@gmail.com>
2022-03-20 20:35:54 -0300

    Added a Future class for showDialog, working as intended

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,26 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-Widget Contain(double height, Widget child, Color color) {
-  return Container(
-    width: 200,
-    child: child,
-    height: height,
-    color: color,
-  );
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyApp createState() => _MyApp();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyApp extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    Widget Contain(double height, Widget child, Color color) {
+      return Container(
+        width: 200,
+        child: child,
+        height: height,
+        color: color,
+      );
+    }
+
     Map<String, String> colorMap = {
       'blue': '4280391411',
       'red': '4294198070',
@@ -44,88 +44,100 @@ class _MyAppState extends State<MyApp> {
     String colorList = cList[Random().nextInt(4)];
 
     bool Test(Color color) {
-      if (colorMap['${colorList}'] == colors.value.toString()) {
+      if (colorMap[colorList] == colors.value.toString()) {
         return true;
       } else {
         return false;
       }
     }
 
-    // Future<void> _showDialog() async {
-    //   await showDialog(
-    //     context: context,
-    //     builder: (ctx) => AlertDialog(
-    //       title: Text('Are you sure?'),
-    //       content: Text('Is that color realy $colorList?'),
-    //       actions: [
-    //         TextButton(
-    //           onPressed: () {
-    //             Navigator.of(context).pop();
-    //           },
-    //           child: Text(
-    //             'Yes',
-    //             style: TextStyle(color: colors),
-    //           ),
-    //         ),
-    //         TextButton(
-    //           onPressed: () {
-    //             Navigator.of(context).pop();
-    //           },
-    //           child: Text(
-    //             'No',
-    //             style: TextStyle(color: colors),
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   );
-    // }
+    Widget Button(onPressed, String text) {
+      return TextButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(color: colors),
+        ),
+      );
+    }
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('First App'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: _rcTap,
-                  child: Contain(200, Text(colorList), colors),
-                ),
+    Future<void> _showMyDialog(BuildContext context) async {
+      return await showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('Is that color realy $colorList?'),
+              actions: <Widget>[
+                Button(() async {
+                  return await showDialog(
+                      context: context,
+                      builder: (context) {
+                        if (Test(colors)) {
+                          return AlertDialog(
+                            content: Text('True'),
+                            actions: [
+                              Button(() {Navigator.of(context).pop();}, 'Nice'),                              
+                            ],                            
+                          );
+                        } else {
+                          return AlertDialog(
+                            content: Text('LIAR!'),
+                            actions: [
+                              Button(() {Navigator.of(context).pop();}, 'Cry'),
+                            ],
+                          );
+                        }
+                      }).then((value) => Navigator.of(context).pop());
+                }, 'Yes'),
+                Button(() {
+                  Navigator.of(context).pop();
+                }, 'No'),
               ],
-            ),
-            Text('Is that color $colorList?'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  child: Text(
-                    'Yes',
-                    style: TextStyle(
-                      color: colors,
-                    ),
+            );
+          });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: _rcTap,
+                child: Contain(200, Text(colorList), colors),
+              ),
+            ],
+          ),
+          Text('Is that color $colorList?'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Button(
+                () {
+                  _showMyDialog(context);
+                },
+                'Yes',
+              ),
+              TextButton(
+                child: Text(
+                  'No',
+                  style: TextStyle(
+                    color: colors,
                   ),
-                  onPressed: () {
-                    // _showDialog();
-                  },
                 ),
-                TextButton(
-                  child: Text(
-                    'No',
-                    style: TextStyle(
-                      color: colors,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
+                onPressed: () {
+                  _rcTap();
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
