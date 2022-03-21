285008c68906bc0dfcc984179130442faf883fea
David Azevedo <davidhazevedog@gmail.com>
2022-03-21 09:27:23 -0300

    New messages for close colors. Final App

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false));
 }
 
 class MyApp extends StatefulWidget {
@@ -21,6 +21,25 @@ class _MyApp extends State<MyApp> {
         color: color,
       );
     }
+
+    Map<String, String> kList = {
+      'red': '4294924066',
+      'green': '4287349578',
+      'blue': '4278430196',
+      // ignore: equal_keys_in_map
+      'blue': '4282339765',
+      // ignore: equal_keys_in_map
+      'blue': '4278238420',
+      'purple': '4284955319',
+    };
+    List<String> mayList = [
+      'Maybe',
+      'Perhaps',
+      'Possibly',
+      'It may be',
+      'It could be',
+      'Perchance',
+    ];
 
     Map<String, String> colorMap = {
       'blue': '4280391411',
@@ -44,7 +63,7 @@ class _MyApp extends State<MyApp> {
     String colorList = cList[Random().nextInt(4)];
 
     bool Test(Color color) {
-      if (colorMap[colorList] == colors.value.toString()) {
+      if (colorMap[colorList] == color.value.toString()) {
         return true;
       } else {
         return false;
@@ -69,30 +88,51 @@ class _MyApp extends State<MyApp> {
               title: Text('Are you sure?'),
               content: Text('Is that color realy $colorList?'),
               actions: <Widget>[
-                Button(() async {
-                  return await showDialog(
-                      context: context,
-                      builder: (context) {
-                        if (Test(colors)) {
-                          return AlertDialog(
-                            content: Text('True'),
-                            actions: [
-                              Button(() {Navigator.of(context).pop();}, 'Nice'),                              
-                            ],                            
-                          );
-                        } else {
-                          return AlertDialog(
-                            content: Text('LIAR!'),
-                            actions: [
-                              Button(() {Navigator.of(context).pop();}, 'Cry'),
-                            ],
-                          );
-                        }
-                      }).then((value) => Navigator.of(context).pop());
-                }, 'Yes'),
-                Button(() {
-                  Navigator.of(context).pop();
-                }, 'No'),
+                Button(
+                  () async {
+                    return await showDialog(
+                        context: context,
+                        builder: (context) {
+                          if (Test(colors)) {
+                            return AlertDialog(
+                              content: Text('True'),
+                              actions: [
+                                Button(() {
+                                  Navigator.of(context).pop();
+                                }, 'Nice'),
+                              ],
+                            );
+                          } else if (kList[colorList] ==
+                              colors.value.toString()) {
+                            return AlertDialog(
+                              content: Text('${mayList[Random().nextInt(5)]}'),
+                              actions: [
+                                Button(() {
+                                  Navigator.of(context).pop();
+                                }, '...'),
+                              ],
+                            );
+                          } else {
+                            return AlertDialog(
+                              content: Text('LIAR!'),
+                              actions: [
+                                Button(() {
+                                  Navigator.of(context).pop();
+                                }, 'Cry'),
+                              ],
+                            );
+                          }
+                        }).then((value) => Navigator.of(context).pop());
+                  },
+                  'Yes',
+                ),
+                Button(
+                  () {
+                    Navigator.of(context).pop();
+                    _rcTap();
+                  },
+                  'No',
+                ),
               ],
             );
           });
@@ -100,7 +140,8 @@ class _MyApp extends State<MyApp> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First App'),
+        title: const Text('Random Color Validator'),
+        backgroundColor: colors,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -110,7 +151,7 @@ class _MyApp extends State<MyApp> {
             children: [
               GestureDetector(
                 onTap: _rcTap,
-                child: Contain(200, Text(colorList), colors),
+                child: Contain(200, Text(''), colors),
               ),
             ],
           ),
@@ -124,16 +165,11 @@ class _MyApp extends State<MyApp> {
                 },
                 'Yes',
               ),
-              TextButton(
-                child: Text(
-                  'No',
-                  style: TextStyle(
-                    color: colors,
-                  ),
-                ),
-                onPressed: () {
+              Button(
+                () {
                   _rcTap();
                 },
+                'No',
               ),
             ],
           ),
